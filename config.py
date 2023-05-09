857006d30ca720030712ced411348ad8e9ae9b14
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-05-09 12:54:13 +0630

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+
+class Config(object):
+      BOT_TOKEN = "5244640547:AAFg7EdXnpugVi2HTR0d9sso-LkFVqMxS_A"
+      API_ID = int("12158462")
+      API_HASH = "0b962717d931f4480c46d56c85d409a5"
+      CAPTION_TEXT = "[👉YN Main Movie ချန်နယ် Joinရန်](https://t.me/YNMOVIETWO)\n[👉YN VIP ချန်နယ် Joinရန်](https://t.me/YN_VIP_Series_ListAndPoster)\n[👉YN Channel Link အားလုံး Joinရန်](https://t.me/YoeNaungAllMovieList/133)\n[🔎Movie တောင်းရန်နှင့်ရှာဖွေရန်ဂရု 🔎](https://t.me/+XTScHquCH0A0ZTQ1)\n[🔎Series တောင်းရန်နှင့်ရှာဖွေရန်ဂရု🔎](https://t.me/YNMovie_SeriesBank)"
+      CAPTION_POSITION = "bottom"
+      ADMIN_USERNAME = "YoeNaung"

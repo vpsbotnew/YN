c0b151fa647e45e7c32a7e897156142a7a89e97b
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-06-12 13:55:43 +0630

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = "6066644150:AAEBFYSGHQSS6e3c0WwDBmdLBn-RtqCV6yE"
       API_ID = int("12158462")
       API_HASH = "0b962717d931f4480c46d56c85d409a5"
-      CAPTION_TEXT = "\n💞 Please Join & Share❗️ကျေးဇူးပါ 💞\n[1️⃣ AtoZ Movie Channel ⭐️](https://t.me/AtoZMovieBank)\n[2️⃣ AtoZ Series Channel ⭐️](https://t.me/MovieSeriesA2Z)\n[3️⃣ ဇာတ်ကား တောင်းရန် / ရှာရန် ဂရု 🔍](https://t.me/+qqQ2cCWKwDtmOGVl)"
+      CAPTION_TEXT = "\n💞 Please Join & Share❗️ကျေးဇူးပါ 💞\n[1️⃣ AtoZ Movie Channel ⭐️](https://t.me/AtoZMovieBank)\n[2️⃣ AtoZ Series Channel ⭐️](https://t.me/MovieSeriesA2Z)\n[3️⃣ ဇာတ်ကား တောင်းရန် / ရှာရန် ဂရု 🔍](https://t.me/+qqQ2cCWKwDtmOGVl)\n[4️⃣ VIP Series မန်ဘာဝင်ရန် 📌](https://t.me/YNVIPMEMBERBOT)"
       CAPTION_POSITION = "bottom"
       ADMIN_USERNAME = "YoeNaung"

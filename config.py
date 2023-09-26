134386fab65ccbd29ca7e2ba808d6f3f52f73df0
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-09-26 14:36:33 +0630

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = "6066644150:AAEBFYSGHQSS6e3c0WwDBmdLBn-RtqCV6yE"
       API_ID = int("20512581")
       API_HASH = "86e23f5f868190ca18c2a542247e51e6"
-      CAPTION_TEXT = "\nဇာတ်ကားစုံစုံလင်လင်ကြည့်ရအောင်\n🟢Main ချန်ယ်ကို Join ထားပေးပါ။\n[1️⃣@YNKSERIESchannel](https://t.me/YoeNaungKSeriesChannel)\n[2️⃣@A2ZAllSERIESchannel](https://t.me/MovieSeriesA2Z)\n\n[🟡ဇာတ်ကားများရှာရန်\n[1️⃣@YNRequestgroup](https://t.me/+XTScHquCH0A0ZTQ1)\n[2️⃣@A2ZRequestgroup](https://t.me/+qqQ2cCWKwDtmOGVl)\n[3️⃣@KSERIESRequestgroup](https://t.me/YNMovie_SeriesBank)"
+      CAPTION_TEXT = "\nဇာတ်ကားစုံစုံလင်လင်ကြည့်ရအောင်\n🟢Main ချန်ယ်ကို Join ထားပေးပါ။\n[1️⃣@YNKSERIESchannel](https://t.me/YoeNaungKSeriesChannel)\n[2️⃣@A2ZAllSERIESchannel](https://t.me/MovieSeriesA2Z)\n\n🟡ဇာတ်ကားများရှာရန်\n[1️⃣@YNRequestgroup](https://t.me/+XTScHquCH0A0ZTQ1)\n[2️⃣@A2ZRequestgroup](https://t.me/+qqQ2cCWKwDtmOGVl)\n[3️⃣@KSERIESRequestgroup](https://t.me/YNMovie_SeriesBank)"
       CAPTION_POSITION = "bottom"
       ADMIN_USERNAME = "YoeNaung"
